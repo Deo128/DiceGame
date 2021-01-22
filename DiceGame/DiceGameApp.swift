f35b540d2f83f634c16da79979ade885083acd44
Dejan Hotic <Dejan.h95@gmail.com>
2021-01-22 13:51:55 +0100

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Dejan Hotic on 2021-01-22.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
